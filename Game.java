ac6b31bc6cba9469f48f6033b3a78564f7bbda33
RanchDressin1211 <reyanshmd@gmail.com>
2024-12-14 13:43:56 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,6 +1,17 @@
 public class Game {
     public static void main(String[] args) throws Exception {
+       String[][] board = new String[7][6];
         System.out.println("Hello, World!");
-        Troop troop = new Troop();
+        int cash = 100;
+        int[] loc = {1,2};
+        Troop troop = new Troop(35,100, loc, "Rifleman");
+        System.out.println(cash);
+    }
+
+    public void addItem(String[][] board, int[]loc, String name){
+        board[loc[0]][loc[1]] = name;
+    }
+    public static void removeItem(String[][] board,int[]loc){
+        board[loc[0]][loc[1]] = "";
     }
 }
